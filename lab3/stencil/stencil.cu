b1e7296583fda79c41e430d677b3c95f7fc93f85
Witalis Domitrz <witekdomitrz@gmail.com>
2022-03-18 17:08:44 +0100

    Work in progress

--- lab3/stencil/stencil.cu
+++ lab3/stencil/stencil.cu
@@ -1,61 +1,78 @@
-#include <time.h>
 #include <stdio.h>
+#include <time.h>
 
-#define RADIUS        3
-#define NUM_ELEMENTS  1000 
+#define RADIUS 3
+#define NUM_ELEMENTS 1000
 
-static void handleError(cudaError_t err, const char *file, int line ) {
-  if (err != cudaSuccess) {
-    printf("%s in %s at line %d\n", cudaGetErrorString(err), file, line);
-    exit(EXIT_FAILURE);
-  }
+static void handleError(cudaError_t err, const char *file, int line) {
+    if (err != cudaSuccess) {
+        printf("%s in %s at line %d\n", cudaGetErrorString(err), file, line);
+        exit(EXIT_FAILURE);
+    }
 }
-#define cudaCheck( err ) (handleError(err, __FILE__, __LINE__ ))
+#define cudaCheck(err) (handleError(err, __FILE__, __LINE__))
 
 __global__ void stencil_1d(int *in, int *out) {
-  //PUT YOUR CODE HERE
+    int i = threadIdx.x + blockIdx.y * gridDim.x;
+
+    out[i] = 0;
+    for (int j = i - RADIUS; j <= i + RADIUS; j++)
+        if (0 <= j && j < NUM_ELEMENTS) out[i] += out[j];
 }
 
 void cpu_stencil_1d(int *in, int *out) {
-  //PUT YOUR CODE HERE
+    for (int i = 0; i < NUM_ELEMENTS; i++) {
+        out[i] = 0;
+        for (int j = i - RADIUS; j <= i + RADIUS; j++)
+            if (0 <= j && j < NUM_ELEMENTS) out[i] += out[j];
+    }
 }
 
 int main() {
-  //PUT YOUR CODE HERE - INPUT AND OUTPUT ARRAYS
-  
-  cudaEvent_t start, stop;
-  cudaEventCreate(&start);
-  cudaEventCreate(&stop);
-  cudaEventRecord( start, 0 );
+    // PUT YOUR CODE HERE - INPUT AND OUTPUT ARRAYS
+    int in[NUM_ELEMENTS], out[NUM_ELEMENTS];
+    int *inGPU, *outGPU;
 
-  //PUT YOUR CODE HERE - DEVICE MEMORY ALLOCATION
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+    cudaEventRecord(start, 0);
 
-  //PUT YOUR CODE HERE - KERNEL EXECUTION
+    // PUT YOUR CODE HERE - DEVICE MEMORY ALLOCATION
+    cudaMalloc((void **)&inGPU, NUM_ELEMENTS * sizeof(int));
+    cudaMalloc((void **)&outGPU, NUM_ELEMENTS * sizeof(int));
 
-  cudaCheck(cudaPeekAtLastError());
+    // Memory initialization
+    cudaMemcpy(inGPU, in, NUM_ELEMENTS * sizeof(int), cudaMemcpyHostToDevice);
 
-  //PUT YOUR CODE HERE - COPY RESULT FROM DEVICE TO HOST
-  
-  cudaEventRecord(stop, 0);
-  cudaEventSynchronize(stop);
-  float elapsedTime;
-  cudaEventElapsedTime( &elapsedTime, start, stop);
-  printf("Total GPU execution time:  %3.1f ms\n", elapsedTime);
-  cudaEventDestroy(start);
-  cudaEventDestroy(stop);
+    // PUT YOUR CODE HERE - KERNEL EXECUTION
 
-  //PUT YOUR CODE HERE - FREE DEVICE MEMORY  
+    cudaCheck(cudaPeekAtLastError());
 
-  struct timespec cpu_start, cpu_stop;
-  clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &cpu_start);
+    // PUT YOUR CODE HERE - COPY RESULT FROM DEVICE TO HOST
+    cudaMemcpy(out, outGPU, NUM_ELEMENTS * sizeof(int), cudaMemcpyHostToDevice);
 
-  cpu_stencil_1d(in, out);
+    cudaEventRecord(stop, 0);
+    cudaEventSynchronize(stop);
+    float elapsedTime;
+    cudaEventElapsedTime(&elapsedTime, start, stop);
+    printf("Total GPU execution time:  %3.1f ms\n", elapsedTime);
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
 
-  clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &cpu_stop);
-  double result = (cpu_stop.tv_sec - cpu_start.tv_sec) * 1e3 + (cpu_stop.tv_nsec - cpu_start.tv_nsec) / 1e6;
-  printf( "CPU execution time:  %3.1f ms\n", result);
-  
-  return 0;
+    // PUT YOUR CODE HERE - FREE DEVICE MEMORY
+    cudaFree(inGPU);
+    cudaFree(outGPU);
+
+    struct timespec cpu_start, cpu_stop;
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &cpu_start);
+
+    cpu_stencil_1d(in, out);
+
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &cpu_stop);
+    double result = (cpu_stop.tv_sec - cpu_start.tv_sec) * 1e3 +
+                    (cpu_stop.tv_nsec - cpu_start.tv_nsec) / 1e6;
+    printf("CPU execution time:  %3.1f ms\n", result);
+
+    return 0;
 }
-
-
