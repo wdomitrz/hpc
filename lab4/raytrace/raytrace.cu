a876f853a45fbe1adb00b6323a2056584ee374f5
Witalis Domitrz <witekdomitrz@gmail.com>
2022-03-25 17:24:52 +0100

    first version

--- lab4/raytrace/raytrace.cu
+++ lab4/raytrace/raytrace.cu
@@ -7,27 +7,28 @@
 #define INF 2e10f
 #define SPHERES 100
 
-struct Sphere {
-    float red, green, blue;
-    float radius;
-    float x, y, z;
+__constant__ float spheres_x[SPHERES], spheres_y[SPHERES], spheres_z[SPHERES],
+    spheres_red[SPHERES], spheres_green[SPHERES], spheres_blue[SPHERES],
+    spheres_radius[SPHERES];
+float hostSpheres_x[SPHERES], hostSpheres_y[SPHERES], hostSpheres_z[SPHERES],
+    hostSpheres_red[SPHERES], hostSpheres_green[SPHERES],
+    hostSpheres_blue[SPHERES], hostSpheres_radius[SPHERES];
 
-    __device__ float hit(float bitmapX, float bitmapY, float *colorFalloff) {
-        float distX = bitmapX - x;
-        float distY = bitmapY - y;
+__device__ float hit(int i, float bitmapX, float bitmapY, float *colorFalloff) {
+    float distX = bitmapX - spheres_x[i];
+    float distY = bitmapY - spheres_y[i];
 
-        if (distX * distX + distY * distY < radius * radius) {
-            float distZ =
-                sqrtf(radius * radius - distX * distX - distY * distY);
-            *colorFalloff = distZ / sqrtf(radius * radius);
-            return distZ + z;
-        }
-
-        return -INF;
+    if (distX * distX + distY * distY < spheres_radius[i] * spheres_radius[i]) {
+        float distZ = sqrtf(spheres_radius[i] * spheres_radius[i] -
+                            distX * distX - distY * distY);
+        *colorFalloff = distZ / spheres_radius[i];
+        return distZ + spheres_z[i];
     }
-};
 
-__global__ void kernel(Sphere *spheres, unsigned char *bitmap) {
+    return -INF;
+}
+
+__global__ void kernel(unsigned char *bitmap) {
     int x = threadIdx.x + blockIdx.x * blockDim.x;
     int y = threadIdx.y + blockIdx.y * blockDim.y;
     int offset = x + y * blockDim.x * gridDim.x;
@@ -40,12 +41,12 @@ __global__ void kernel(Sphere *spheres, unsigned char *bitmap) {
 
     for (int i = 0; i < SPHERES; i++) {
         float colorFalloff;
-        float depth = spheres[i].hit(bitmapX, bitmapY, &colorFalloff);
+        float depth = hit(i, bitmapX, bitmapY, &colorFalloff);
 
         if (depth > maxDepth) {
-            red = spheres[i].red * colorFalloff;
-            green = spheres[i].green * colorFalloff;
-            blue = spheres[i].blue * colorFalloff;
+            red = spheres_red[i] * colorFalloff;
+            green = spheres_green[i] * colorFalloff;
+            blue = spheres_blue[i] * colorFalloff;
             maxDepth = depth;
         }
     }
@@ -58,7 +59,7 @@ __global__ void kernel(Sphere *spheres, unsigned char *bitmap) {
 
 struct DataBlock {
     unsigned char *hostBitmap;
-    Sphere *spheres;
+    // Sphere *spheres;
 };
 
 int main(void) {
@@ -70,30 +71,41 @@ int main(void) {
 
     CPUBitmap bitmap(DIM, DIM, &data);
     unsigned char *devBitmap;
-    Sphere *devSpheres;
+    // Sphere *devSpheres;
 
     HANDLE_ERROR(cudaMalloc((void **)&devBitmap, bitmap.image_size()));
-    HANDLE_ERROR(cudaMalloc((void **)&devSpheres, sizeof(Sphere) * SPHERES));
+    // HANDLE_ERROR(cudaMalloc((void **)&devSpheres, sizeof(Sphere) * SPHERES));
 
-    Sphere *hostSpheres = (Sphere *)malloc(sizeof(Sphere) * SPHERES);
+    // Sphere *hostSpheres = (Sphere *)malloc(sizeof(Sphere) * SPHERES);
 
     for (int i = 0; i < SPHERES; i++) {
-        hostSpheres[i].red = rnd(1.0f);
-        hostSpheres[i].green = rnd(1.0f);
-        hostSpheres[i].blue = rnd(1.0f);
-        hostSpheres[i].x = rnd(1000.0f) - 500;
-        hostSpheres[i].y = rnd(1000.0f) - 500;
-        hostSpheres[i].z = rnd(1000.0f) - 500;
-        hostSpheres[i].radius = rnd(100.0f) + 20;
+        hostSpheres_red[i] = rnd(1.0f);
+        hostSpheres_green[i] = rnd(1.0f);
+        hostSpheres_blue[i] = rnd(1.0f);
+        hostSpheres_x[i] = rnd(1000.0f) - 500;
+        hostSpheres_y[i] = rnd(1000.0f) - 500;
+        hostSpheres_z[i] = rnd(1000.0f) - 500;
+        hostSpheres_radius[i] = rnd(100.0f) + 20;
     }
 
-    HANDLE_ERROR(cudaMemcpy(devSpheres, hostSpheres, sizeof(Sphere) * SPHERES,
-                            cudaMemcpyHostToDevice));
-    free(hostSpheres);
+    HANDLE_ERROR(
+        cudaMemcpyToSymbol(spheres_x, hostSpheres_x, sizeof(float) * SPHERES));
+    HANDLE_ERROR(
+        cudaMemcpyToSymbol(spheres_y, hostSpheres_y, sizeof(float) * SPHERES));
+    HANDLE_ERROR(
+        cudaMemcpyToSymbol(spheres_z, hostSpheres_z, sizeof(float) * SPHERES));
+    HANDLE_ERROR(cudaMemcpyToSymbol(spheres_radius, hostSpheres_radius,
+                                    sizeof(float) * SPHERES));
+    HANDLE_ERROR(cudaMemcpyToSymbol(spheres_blue, hostSpheres_blue,
+                                    sizeof(float) * SPHERES));
+    HANDLE_ERROR(cudaMemcpyToSymbol(spheres_green, hostSpheres_green,
+                                    sizeof(float) * SPHERES));
+    HANDLE_ERROR(cudaMemcpyToSymbol(spheres_red, hostSpheres_red,
+                                    sizeof(float) * SPHERES));
 
     dim3 grids(DIM / 16, DIM / 16);
     dim3 threads(16, 16);
-    kernel<<<grids, threads>>>(devSpheres, devBitmap);
+    kernel<<<grids, threads>>>(devBitmap);
 
     HANDLE_ERROR(cudaEventRecord(stop, 0));
     HANDLE_ERROR(cudaEventSynchronize(stop));
@@ -111,5 +123,4 @@ int main(void) {
     // bitmap.dump_ppm("image.ppm");
 
     cudaFree(devBitmap);
-    cudaFree(devSpheres);
 }
