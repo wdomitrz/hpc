422e668867d809cc87c92c139d221994d0d59628
Witalis Domitrz <witekdomitrz@gmail.com>
2022-04-01 16:51:51 +0200

    plm done

--- lab5/plm/plm.cu
+++ lab5/plm/plm.cu
@@ -10,18 +10,20 @@ float cuda_malloc_test(int size, bool up) {
     HANDLE_ERROR(cudaEventCreate(&start));
     HANDLE_ERROR(cudaEventCreate(&stop));
 
-    a = (int*)malloc(size * sizeof(*a)); 
+    a = (int *)malloc(size * sizeof(*a));
 
     HANDLE_NULL(a);
-    HANDLE_ERROR(cudaMalloc((void**)&dev_a, size * sizeof(*dev_a)));
+    HANDLE_ERROR(cudaMalloc((void **)&dev_a, size * sizeof(*dev_a)));
 
-    HANDLE_ERROR(cudaEventRecord(start, 0)); 
+    HANDLE_ERROR(cudaEventRecord(start, 0));
 
     for (int i = 0; i < 100; i++) {
         if (up) {
-            HANDLE_ERROR(cudaMemcpy(dev_a, a, size * sizeof(*dev_a), cudaMemcpyHostToDevice));
+            HANDLE_ERROR(cudaMemcpy(dev_a, a, size * sizeof(*dev_a),
+                                    cudaMemcpyHostToDevice));
         } else {
-            HANDLE_ERROR(cudaMemcpy(a, dev_a, size * sizeof(*dev_a), cudaMemcpyDeviceToHost));
+            HANDLE_ERROR(cudaMemcpy(a, dev_a, size * sizeof(*dev_a),
+                                    cudaMemcpyDeviceToHost));
         }
     }
 
@@ -34,7 +36,44 @@ float cuda_malloc_test(int size, bool up) {
     HANDLE_ERROR(cudaEventDestroy(start));
     HANDLE_ERROR(cudaEventDestroy(stop));
 
-    return elapsedTime; 
+    return elapsedTime;
+}
+
+float cuda_cudaHostAlloc_test(int size, bool up) {
+    cudaEvent_t start, stop;
+    int *a, *dev_a;
+    float elapsedTime;
+
+    HANDLE_ERROR(cudaEventCreate(&start));
+    HANDLE_ERROR(cudaEventCreate(&stop));
+
+    HANDLE_ERROR(cudaHostAlloc(&a, size * sizeof(*a), cudaHostAllocDefault));
+
+    HANDLE_NULL(a);
+    HANDLE_ERROR(cudaMalloc((void **)&dev_a, size * sizeof(*dev_a)));
+
+    HANDLE_ERROR(cudaEventRecord(start, 0));
+
+    for (int i = 0; i < 100; i++) {
+        if (up) {
+            HANDLE_ERROR(cudaMemcpy(dev_a, a, size * sizeof(*dev_a),
+                                    cudaMemcpyHostToDevice));
+        } else {
+            HANDLE_ERROR(cudaMemcpy(a, dev_a, size * sizeof(*dev_a),
+                                    cudaMemcpyDeviceToHost));
+        }
+    }
+
+    HANDLE_ERROR(cudaEventRecord(stop, 0));
+    HANDLE_ERROR(cudaEventSynchronize(stop));
+    HANDLE_ERROR(cudaEventElapsedTime(&elapsedTime, start, stop));
+
+    cudaFree(a);
+    HANDLE_ERROR(cudaFree(dev_a));
+    HANDLE_ERROR(cudaEventDestroy(start));
+    HANDLE_ERROR(cudaEventDestroy(stop));
+
+    return elapsedTime;
 }
 
 int main(void) {
@@ -43,13 +82,29 @@ int main(void) {
 
     elapsedTime = cuda_malloc_test(SIZE, true);
 
-    printf("Total time for copy up: %3.1f ms\n", elapsedTime);
-    printf("\tMB/s during copy up:  %3.1f\n", MB / (elapsedTime / 1000));
+    printf("Total time for copy up with malloc: %3.1f ms\n", elapsedTime);
+    printf("\tMB/s during copy up with malloc:  %3.1f\n",
+           MB / (elapsedTime / 1000));
 
-    elapsedTime = cuda_malloc_test(SIZE, false); 
-    
-    printf("Total time for copy down: %3.1f ms\n", elapsedTime);
-    printf("\tMB/s during copy down:  %3.1f\n", MB / (elapsedTime / 1000));
+    elapsedTime = cuda_malloc_test(SIZE, false);
+
+    printf("Total time for copy down with malloc: %3.1f ms\n", elapsedTime);
+    printf("\tMB/s during copy down with malloc:  %3.1f\n",
+           MB / (elapsedTime / 1000));
+
+    elapsedTime = cuda_cudaHostAlloc_test(SIZE, true);
+
+    printf("Total time for copy up with cudaMemcpyAsync: %3.1f ms\n",
+           elapsedTime);
+    printf("\tMB/s during copy up with cudaMemcpyAsync:  %3.1f\n",
+           MB / (elapsedTime / 1000));
+
+    elapsedTime = cuda_cudaHostAlloc_test(SIZE, false);
+
+    printf("Total time for copy down with cudaMemcpyAsync: %3.1f ms\n",
+           elapsedTime);
+    printf("\tMB/s during copy down with cudaMemcpyAsync:  %3.1f\n",
+           MB / (elapsedTime / 1000));
 
     return 0;
 }
