0d901b6370db5224c349521325eee37ca238ffec
Witalis Domitrz <witekdomitrz@gmail.com>
2022-05-20 16:46:36 +0200

    tbb 1 parfor

--- tbb-lab-01/parfor.cpp
+++ tbb-lab-01/parfor.cpp
@@ -3,20 +3,19 @@
 // HPC course, MIM UW
 // Krzysztof Rzadca, LGPL
 
-#include "tbb/tbb.h"
-#include <iostream>
 #include <math.h>
 
+#include <iostream>
+
+#include "tbb/tbb.h"
 
 bool is_prime(long num) {
-    long sqrt_num = (long) sqrt(num);
+    long sqrt_num = (long)sqrt(num);
     for (long div = 2; div <= sqrt_num; ++div) {
-        if ((num % div) == 0)
-            return false;
+        if ((num % div) == 0) return false;
     }
     return true;
 }
-
 
 int main() {
     const long limit = 5'000'000;
@@ -25,19 +24,43 @@ int main() {
         is_prime(i);
     }
     tbb::tick_count seq_end_time = tbb::tick_count::now();
-    double seq_time = (seq_end_time-seq_start_time).seconds();
-    std::cout << "seq time for "<< limit <<" " << seq_time << "[s]" <<std::endl;
+    double seq_time = (seq_end_time - seq_start_time).seconds();
+    std::cout << "seq time for " << limit << " " << seq_time << "[s]"
+              << std::endl;
 
+#ifdef NO_ATOMIC
+    long primes_counter = 0;
+#else
+    std::atomic<long> primes_counter(0);
+#endif
+    std::atomic<long long> ran_len_sq(0), ran_numb(0);
     tbb::tick_count par_start_time = tbb::tick_count::now();
-    tbb::parallel_for(  // execute a parallel for
+    tbb::parallel_for(                       // execute a parallel for
         tbb::blocked_range<long>(1, limit),  // pon a range from 1 to limit
-        [](const tbb::blocked_range<long>& r) { // inside a loop, for a partial range r,
+        [&primes_counter, &ran_len_sq,
+         &ran_numb](const tbb::blocked_range<long>&
+                        r) {  // inside a loop, for a partial range r,
+            long long len = r.end() - r.begin();
+            ran_len_sq += len * len;
+            ran_numb++;
+            long local_counter = 0;
             // run this lambda
-            for (long i = r.begin(); i != r.end(); ++i)
-                is_prime(i);
-        }
-        );
+            for (long i = r.begin(); i != r.end(); ++i) {
+                if (is_prime(i)) local_counter++;
+            }
+            primes_counter += local_counter;
+        });
     tbb::tick_count par_end_time = tbb::tick_count::now();
-    double par_time = (par_end_time-par_start_time).seconds();
-    std::cout << "par time for "<< limit <<" " << par_time << "[s]" <<std::endl;
+    double par_time = (par_end_time - par_start_time).seconds();
+    std::cout << "par time for " << limit << " " << par_time << "[s]"
+              << std::endl;
+    std::cout << "primes count:" << primes_counter << "\n";
+
+    std::cout << "numer of ranges: " << ran_numb << "\n"
+              << "mean len: " << ((double)limit) / ran_numb << "\n"
+              << "std len: "
+              << sqrt(((double)ran_len_sq) / ran_numb -
+                      (((double)limit) / ran_numb) *
+                          (((double)limit) / ran_numb))
+              << "\n";
 }
